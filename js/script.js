00363963a6d3db3e57798075e3e7288df0f4915c
Dimitar Panayotov <dpanayotov93@gmail.com>
2015-07-21 00:31:28 +0300

    Base development
    Base development

--- js/script.js
+++ js/script.js
@@ -5,45 +5,31 @@ function httpGet(theUrl) {
 	return xmlHttp.responseText;
 }
 
-function mover(img1){
+function mover(img1) {
 	//Define out dimensions
 	W = "200px";
 	H = "270px";
-	
+
 	//Create our new container <DIV> and its attributes
 	div = document.createElement('div');
-	div.setAttribute("id","grow");
+	div.setAttribute("id", "grow");
 	div.style.position = "absolute";
 	div.style.width = W;
 	div.style.height = H;
 	div.style.margin = "30px 70px";
-	div.style.boxShadow = "0px 0px 30px #9bebff"; 
-	
+	div.style.boxShadow = "0px 0px 30px #9bebff";
+
 	//Create out new magnified <IMG> and its attributes
-	img = document.createElement("img");	
+	img = document.createElement("img");
 	img.src = img1.src;
 	img.style.width = W;
 	img.style.height = H;
-	
+
 	//Append the <IMG> to the <DIV>
-	div.appendChild(img);	
-	
+	div.appendChild(img);
+
 	//Append the <DIV> to the document
 	document.body.appendChild(div);
-}
-
-function removeLands(){
-	var i, id, type, cardName, result;
-	for (i = 0; i < set.length; i+=1) {
-		id = set[i];
-		cardName = httpGet('http://api.mtgdb.info/cards/' + id);
-		result = cardName.match("type\":\"(.*)\",\"subType");                   
-		type = result[1];
-		if (type === 'Land' || type === 'Basic Land') {
-			console.log('Land');
-			set.splice(i, 1);
-		}
-	}
 }
 
 function getSet(name) {
@@ -55,37 +41,37 @@ function getSet(name) {
 }
 
 function countInArray(array, value) {
-    var count = 0;
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] === value) {
-            count++;
-        }
-    }
-    console.log(count);
-    return count;
+	var count = 0;
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] === value) {
+			count++;
+		}
+	}
+	console.log(count);
+	return count;
 }
 
 function sortDivs() {
 	var main = document.getElementById('marketCards');
 
-	[].map.call( main.children, Object ).sort( function ( a, b ) {
-	    return +a.id.match( /\d+/ ) - +b.id.match( /\d+/ );
-	}).forEach( function ( elem ) {
-	    main.appendChild( elem );
+	[].map.call(main.children, Object).sort(function(a, b) {
+		return +a.id.match(/\d+/) - +b.id.match(/\d+/);
+	}).forEach(function(elem) {
+		main.appendChild(elem);
 	});
 }
 
-function listMarket(){
- 	removeLands();
-	document.getElementById('marketCards').innerHTML = ''; 
+function listMarket() {
+	document.getElementById('marketCards').innerHTML = '';
 	for (var i = 0; i < set.length; i++) {
 		var id = set[i];
 		document.getElementById('marketCards').innerHTML += '<img " onclick="add(' + id + ')" id="' + id + '" src="http://api.mtgdb.info/content/hi_res_card_images/' + id + '.jpg">';
 	}
 	sortDivs();
+	show('loading', false);
 }
 
-function updateMarket(id){
+function updateMarket(id) {
 	var img = "#" + id;
 	if ($("#marketCards").find(img).length === 0) {
 		set.push(id);
@@ -94,7 +80,7 @@ function updateMarket(id){
 	}
 }
 
-function listDeck(){
+function listDeck() {
 	document.getElementById('libraryCards').innerHTML = '';
 	for (var i = 0; i < deck.length; i++) {
 		document.getElementById('libraryCards').innerHTML += '<img " onclick="del(' + deck[i] + ')" id="' + deck[i] + '" src="http://api.mtgdb.info/content/hi_res_card_images/' + deck[i] + '.jpg">';
@@ -119,7 +105,7 @@ function add(id) {
 	} else if (count === 3) {
 		deck.push(id);
 		var index = set.indexOf(id);
-		set.splice(index, 1);		
+		set.splice(index, 1);
 		document.getElementById(id).remove();
 		listDeck();
 	}
@@ -133,52 +119,54 @@ function saveDeck() {
 	var encodedUri = encodeURI(csvContent);
 	var link = document.createElement("a");
 	link.setAttribute("href", encodedUri);
-	link.setAttribute("download", "my_deck.csv");
+	$.prompt('<label>Name: <input type="text" name="name" value=""></label><br />', {
+				title: "SAVE DECK",
+				buttons: {"Save": true, "Back": false},
+				submit:function(e,v,m,f){
+					if (v === true) {
+						link.setAttribute("download", f.name + ".csv");
+						link.click();
+					} else {
+						$.prompt.close();
+					}
+				}
+			});
 
-	link.click();
+}
+
+function changeSet(name) {
+	show('loading', true);
+	console.log(name);
+	set = getSet(name);
+	setTimeout(function() {
+		listMarket();
+	}, 3000);
 }
 
 function loadDeck() {
-	var newDeck = [];
-	var name = 'my_data.csv';
-	loadCSV(name);
-	console.log(arr);
+	document.getElementById('fileInput').click();
 }
 
-function loadCSV(file) {
-    var request;
-    if (window.XMLHttpRequest) {
-        // IE7+, Firefox, Chrome, Opera, Safari
-        request = new XMLHttpRequest();
-    } else {
-        // code for IE6, IE5
-        request = new ActiveXObject('Microsoft.XMLHTTP');
-    }
-    // load
-    request.open('GET', file, false);
-    request.send();
-    return request.responseText;
+function handleFiles(files) {
+	var file = files[0];
+	var reader = new FileReader();
+	reader.onload = onFileReadComplete;
+	reader.readAsText(file);
 }
 
+function onFileReadComplete(event) {
+	deck = event.target.result.split(',');
+	listDeck();
+}
 
 function testUI() {
 	set = getSet('frf');
-	listMarket();
+	// listMarket();
 	listDeck();
 	sortDivs();
 
-
-	var cardName = 'phalanx leader';
-	// $('#name').append(objects.card.name);
-	// $('#mana').append(objects.card.id);
-	// $('#oracle').append(objects.card.oracle);
-	$('#name').append(getName(cardName));
-	$('#mana').append(getMana(cardName));
-	$('#oracle').append(getOracle(cardName));
-	// $('#test').append(card.mana);
-
 	// console.log(httpGet('http://api.mtgdb.info/cards/' + cardName));
-	$('#debug').append(httpGet('http://api.mtgdb.info/cards/' + cardName));
+	// $('#debug').append(httpGet('http://api.mtgdb.info/cards/' + cardName));
 }
 
 function getName(cardName) {
@@ -198,3 +186,25 @@ function getOracle(cardName) {
 	var result = name.match("description\":\"(.*)\",\"flavor\"");
 	return result.toString().substring(14);
 }
+	var checked = [],
+		unchecked = [];
+
+function onReady(callback) {
+	var intervalID = window.setInterval(checkReady, 1000);
+
+	function checkReady() {
+		if (document.getElementsByTagName('body')[0] !== undefined) {
+			window.clearInterval(intervalID);
+			callback.call(this);
+		}
+	}
+}
+
+function show(id, value) {
+	document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function() {
+	// show('page', true);
+	show('loading', false);
+});
